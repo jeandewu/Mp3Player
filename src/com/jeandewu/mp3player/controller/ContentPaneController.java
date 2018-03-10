950f64dac09ae33f2997882f410fdc90ff0bf662
jeandewu <jeandewu@gmail.com>
2018-03-10 21:46:05 +0100

    v.0.7 - dodane Java ID3 Tag Library

--- src/com/jeandewu/mp3player/controller/ContentPaneController.java
+++ src/com/jeandewu/mp3player/controller/ContentPaneController.java
@@ -1,20 +1,40 @@
 package com.jeandewu.mp3player.controller;
-
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import com.jeandewu.mp3player.mp3.Mp3Song;
 
-public class ContentPaneController implements Initializable{
+public class ContentPaneController implements Initializable {
+
+    public static final String TITLE_COLUMN = "Tytuł";
+    public static final String AUTHOR_COLUMN = "Autor";
+    public static final String ALBUM_COLUMN = "Album";
 
     @FXML
-    private TableView<?> contentTable;
+    private TableView<Mp3Song> contentTable;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
+        configureTable();
+    }
+
+    private void configureTable() {
+        TableColumn<Mp3Song, String> titleColumn = new TableColumn<Mp3Song, String>(TITLE_COLUMN);
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+
+        TableColumn<Mp3Song, String> authorColumn = new TableColumn<Mp3Song, String>(AUTHOR_COLUMN);
+        authorColumn.setCellValueFactory(new PropertyValueFactory<>("author"));
+
+        TableColumn<Mp3Song, String> albumColumn = new TableColumn<Mp3Song, String>(ALBUM_COLUMN);
+        albumColumn.setCellValueFactory(new PropertyValueFactory<>("album"));
+
+        contentTable.getColumns().add(titleColumn);
+        contentTable.getColumns().add(authorColumn);
+        contentTable.getColumns().add(albumColumn);
     }
 }
