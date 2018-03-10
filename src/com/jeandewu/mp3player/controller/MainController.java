0d16504e0cbf9d70673b17d6d2d421de4d743ad3
jeandewu <jeandewu@gmail.com>
2018-03-10 12:42:58 +0100

    v.0.4 - obsługa zdarzeń przycisków

--- src/com/jeandewu/mp3player/controller/MainController.java
+++ src/com/jeandewu/mp3player/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.jeandewu.mp3player.controller;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -11,6 +14,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 
 public class MainController implements Initializable {
 
@@ -52,8 +56,37 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
+        configureButtons();
+        configureVolume();
+    }
 
+    private void configureVolume() {
+        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("Wciśnięto przycisk na suwaku głośności");
+            }
+        });
+    }
+
+    private void configureButtons() {
+        prevButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Poprzednia piosenka");
+            }
+        });
+        nextButton.setOnAction(x -> System.out.println("Następna piosenka"));
+        playButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(playButton.isSelected()) {
+                    System.out.println("Play");
+                } else {
+                    System.out.println("Stop");
+                }
+            }
+        });
     }
 
 }
