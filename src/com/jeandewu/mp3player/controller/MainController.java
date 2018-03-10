c10056fc3e3e0494211e850aab70cdbb5483eb0d
jeandewu <jeandewu@gmail.com>
2018-03-10 13:00:18 +0100

    v.0.5 - obsługa zdarzeń przycisków, rozbicie widoków

--- src/com/jeandewu/mp3player/controller/MainController.java
+++ src/com/jeandewu/mp3player/controller/MainController.java
@@ -1,92 +1,15 @@
 package com.jeandewu.mp3player.controller;
 
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.input.MouseEvent;
 
 public class MainController implements Initializable {
 
-    @FXML
-    private Button nextButton;
-
-    @FXML
-    private Slider volumeSlider;
-
-    @FXML
-    private Slider songSlider;
-
-    @FXML
-    private MenuItem aboutMenuItem;
-
-    @FXML
-    private TableView<?> contentTable;
-
-    @FXML
-    private MenuItem fileMenuItem;
-
-    @FXML
-    private MenuItem closeMenuItem;
-
-    @FXML
-    private MenuItem dirMenuItem;
-
-    @FXML
-    private Menu helpMenu;
-
-    @FXML
-    private Button prevButton;
-
-    @FXML
-    private ToggleButton playButton;
-
-    @FXML
-    private Menu fileMenu;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        configureButtons();
-        configureVolume();
     }
-
-    private void configureVolume() {
-        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                System.out.println("Wciśnięto przycisk na suwaku głośności");
-            }
-        });
-    }
-
-    private void configureButtons() {
-        prevButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Poprzednia piosenka");
-            }
-        });
-        nextButton.setOnAction(x -> System.out.println("Następna piosenka"));
-        playButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if(playButton.isSelected()) {
-                    System.out.println("Play");
-                } else {
-                    System.out.println("Stop");
-                }
-            }
-        });
-    }
-
 }
